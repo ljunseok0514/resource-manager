448026dbcef45f350de9c7ea74c1307223d7bb55
ljunseok0514 <73566234+ljunseok0514@users.noreply.github.com>
2023-02-08 17:52:24 +0900

    [230208 물건추가 ui,증감버튼 개발]

--- src/components/pageContents/ResourceDetailPageContents/ResourceDetailPageContents.jsx
+++ src/components/pageContents/ResourceDetailPageContents/ResourceDetailPageContents.jsx
@@ -7,11 +7,47 @@ import classNames from "classnames/bind";
 import style from "./style.module.scss";
 const cx = classNames.bind(style);
 
+function BoxItem() {
+  const [count, setCount] = useState(0);
+  const onCountPlus = () => {
+    setCount(count + 1);
+  };
+  const onCountMinus = () => {
+    setCount(count - 1);
+  };
+  return (
+    <div className={cx("box_item")}>
+      <div className={cx("box_item_info")}>
+        <div className={cx("box_item_img")}>
+          +
+          <img src="hwayo.png" alt="" />
+        </div>
+        <div className={cx("box_item_title")}>
+          <input type="text" />
+        </div>
+      </div>
+      <div className={cx("box_item_count")}>
+        <div className={cx("box_item_count_btn")} onClick={onCountPlus}>
+          +
+        </div>
+        <div className={cx("box_item_count_num")}>{count}</div>
+        <div className={cx("box_item_count_btn")} onClick={onCountMinus}>
+          -
+        </div>
+      </div>
+    </div>
+  );
+}
+
 function ResourceDetailPageContents() {
   const { id } = useParams();
   const [boxName, setBoxName] = useRecoilState(boxTitle);
   const onChangeBox = (e) => {
     setBoxName(e.target.value);
+  };
+  const [isLocker, setIsLocker] = useState(false);
+  const onClickAdd = () => {
+    setIsLocker(true);
   };
 
   return (
@@ -23,18 +59,10 @@ function ResourceDetailPageContents() {
           className={cx("box_title")}
           placeholder="새 사물함"
         />
-        <div className={cx("box_item_btn")}>+물건 추가하기</div>
-        <div className={cx("box_item")}>
-          <div className={cx("box_item_info")}>
-            <div className={cx("box_item_img")}></div>
-            <div className={cx("box_item_title")}>라면</div>
-          </div>
-          <div className={cx("box_item_count")}>
-            <div className={cx("box_item_count_btn")}>+</div>
-            <div className={cx("box_item_count_btn")}>20</div>
-            <div className={cx("box_item_count_btn")}>-</div>
-          </div>
+        <div className={cx("box_item_btn")} onClick={onClickAdd}>
+          +물건 추가하기
         </div>
+        {isLocker && <BoxItem />}
       </div>
     </div>
   );
